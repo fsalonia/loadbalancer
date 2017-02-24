e1e6965db26fbe21e8570661c076f92153ae82b3
Franco Salonia <francosalonia@MacBook-Air-de-Franco.local>
2017-02-24 00:24:27 -0300

    Added confd interval and hooks

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 rm /etc/nginx/conf.d/default.conf
+echo "Starting etcd for initial configuration"
 /confd -onetime -backend etcd -node ${ETCD_IP}:2379
-tail -f /dev/null
+echo "Put a continual polling confd process into the background to watch every 10 seconds"
+/confd -interval -backend etcd -node ${ETCD_IP}:2379 &
+tail -f /var/log/nginx/*.log
